9bd3194310876d3e2c9eef640e64beafa6aa033c
kachi <astrokachi@gmail.com>
2026-06-11 18:36:22 +0100

    add wllama library + configure webpack for wasm support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  serverExternalPackages: ['pdf-parse']
+  serverExternalPackages: ['pdf-parse', '@wllama/wllama'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@wllama/wllama$': '@wllama/wllama/esm/index.js',
+      };
+    } else {
+      if (!config.externals) {
+        config.externals = [];
+      }
+      if (Array.isArray(config.externals)) {
+        config.externals.push('@wllama/wllama');
+      }
+    }
+
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'webassembly/async',
+    });
+
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
